3140370a5568d3396e3eb76e96a60998cb6036fe
曹得地 <gw00212024@corp.gwm.cn>
2020-11-28 08:46:53 +0800

    c

---
+++ Example/Pods/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Pods
+//
+//  Created by CG on 2020/11/27.
+//
+
+import Foundation
--- Example/tester.swift
+++
@@ -1,9 +0,0 @@
-//
-//  tester.swift
-//  CGReSos_Tests
-//
-//  Created by CG on 2020/11/27.
-//  Copyright © 2020 CocoaPods. All rights reserved.
-//
-
-import Foundation
